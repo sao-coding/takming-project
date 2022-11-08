baec2f15e24a4838906bdecfa54aeb8c63c1486c
sao-coding <58339015+sao-coding@users.noreply.github.com>
2022-11-08 12:53:00 +0000

    新增 資料

--- website/models.py
+++ website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20, null=False)
+    password = models.CharField(max_length=20, null=False)
+    # email = models.EmailField()
+    # phone = models.CharField(max_length=10)
+    # address = models.CharField(max_length=100)
+    # birthday = models.DateField()
+    enabled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
